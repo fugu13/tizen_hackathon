a01fc24ac71e01a865f7c4f2b0ec906bf4e69db7
Russell Duhon <fugu13@gmail.com>
2013-12-07 14:26:41 -0800

    new version of app using jqm

---
+++ js/main.js
@@ -0,0 +1,77 @@
+var backEventListener = null;
+
+var unregister = function() {
+    if ( backEventListener !== null ) {
+        document.removeEventListener( 'tizenhwkey', backEventListener );
+        backEventListener = null;
+        window.tizen.application.getCurrentApplication().exit();
+    }
+}
+
+//Initialize function
+var init = function () {
+    // register once
+    if ( backEventListener !== null ) {
+        return;
+    }
+    
+    // TODO:: Do your initialization job
+    console.log("init() called");
+    
+    var backEvent = function(e) {
+        if ( e.keyName == "back" ) {
+            try {
+                if ( $.mobile.urlHistory.activeIndex <= 0 ) {
+                    // if first page, terminate app
+                    unregister();
+                } else {
+                    // move previous page
+                    $.mobile.urlHistory.activeIndex -= 1;
+                    $.mobile.urlHistory.clearForward();
+                    window.history.back();
+                }
+            } catch( ex ) {
+                unregister();
+            }
+        }
+    }
+    
+    // add eventListener for tizenhwkey (Back Button)
+    document.addEventListener( 'tizenhwkey', backEvent );
+    backEventListener = backEvent;
+    
+    var database = {};
+    
+    $('.make').bind('click', function() {
+    	var textarea = $('#write');
+    	//textarea.addClass('visible');
+    	textarea.focus();
+    });
+    var makeNoteItem = function(text) {
+    	return '<li>' + text + '</li>';
+    };
+    var note = function() {
+    	var textarea = $('#write');
+    	var text = textarea.val()
+    	if(text) {
+    		$('#notes').append(makeNoteItem(text)).listview('refresh');
+    	}
+    	//textarea.removeClass('visible');
+    	textarea.val('');
+    };
+    $('#content').on('keypress', '#write.visible', function(e) {
+    	var code = e.keyCode || e.which;
+    	if(code == 13) {
+    		note();
+    	}
+    });
+    $('#participants').on('click', 'a', function() {
+    	database.current = $(this).text();
+    	console.log("Current!", database.current);
+    	$('#notes').append('<li data-role="list-divider">' + database.current + '</li>').listview('refresh');
+    	$('#content').tap();
+    });
+};
+
+$(document).bind( 'pageinit', init );
+$(document).unload( unregister );
